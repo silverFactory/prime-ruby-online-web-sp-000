1f54be30cbf0bb598c0235f8649dd4767a61d123
Will Studabaker <will.studabaker@gmail.com>
2020-10-28 13:02:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(int)
+  limit = Math.sqrt(int)
+  num_check = 2
+  while num_check <= limit do
+   if int.modulo(num_check) == 0
+     return true
+   else
+     num_check += 1
+  end
+end
